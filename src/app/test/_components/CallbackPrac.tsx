663c46f82e03b89e1772dd7978bd86fb4fa360b8
Mahfuz Hasan <mahfuz15-11584@diu.edu.bd>
2025-06-09 21:19:45 +0600

    initial code review with packages installed

---
+++ src/app/test/_components/CallbackPrac.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+const CallbackPractice = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => setCount((prev) => prev + 1), []);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default CallbackPractice;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import CallbackPractice from "./_components/CallbackPrac";
+
+const page = () => {
+  return (
+    <div>
+      <CallbackPractice />
+    </div>
+  );
+};
+
+export default page;
